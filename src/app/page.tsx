35b9d3dcc05a703f519ea9305ca57f8ada297736
Zahra shahriaripour <zahra.shahriaripour8285@gmail.com>
2025-11-01 23:12:51 +0330

    react hook form added to signup page with successfully

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Toaster } from "react-hot-toast";
 
 
 export default function Home() {
@@ -5,6 +6,7 @@ export default function Home() {
   
       <main >
         <h1>سلام</h1>
+        <Toaster/>
       </main>
     
   );
--- src/components/template/authForm/SignupPage.tsx
+++ src/components/template/authForm/SignupPage.tsx
@@ -1,6 +1,14 @@
-  import { useForm, SubmitHandler } from "react-hook-form"
+  "use client"
+
+import { useForm, SubmitHandler } from "react-hook-form"
 import style from "../authForm/SignUpPage.module.css"
+import { useRouter } from "next/navigation"
+import toast from "react-hot-toast"
 import Link from "next/link"
+
+
+
+
 
 
 
@@ -11,29 +19,49 @@ type Inputs = {
 }
 
 function SignupPage() {
-
+  const router = useRouter();
  const {
     register,
     handleSubmit,
-    watch,
+   watch,
     formState: { errors },
   } = useForm<Inputs>()
 
+ const password = watch('password');
 
-const submitHandler =()=>{
+const submitHandler:SubmitHandler<Inputs> =async ({email,password})=>{
+  
+  const res= await fetch("/api/auth/signup",{
+    method:"POST",
+    body:JSON.stringify({email,password}),
+    headers:{ "Content-Type": "application/json" }
+  });
 
-}
+  const data= await res.json()
+if (res.status === 201) {
+      router.push("/signin");
+    } else {
+      toast.error(data.error);
+    }
+  };
+
+
+
 
   return (
     <div className={style.form}>
       <h4>فرم ثبت نام</h4>
       <form onSubmit={handleSubmit(submitHandler)}>
         <label>ایمیل :</label>
+        {errors.email && <span className={style.error}>ایمیل الزامی است</span>}
         <input type="text"  {...register("email",{ required: true })} />
         <label>رمز عبور :</label> 
+           {errors.password && <span className={style.error}>رمز عبور الزامی است</span>}
         <input type="password" {...register("password",{required:true})} />
          <label> تکرار رمز  عبور :</label> 
-        <input type="password" {...register("rePassword",{required:true})}  />
+          {errors.rePassword && <span className={style.error}>{errors.rePassword.message}</span>}
+        <input type="password" {...register("rePassword",{required:"تکرار رمز عبور الزامی است",   validate: value => value ===password  || 'رمز عبور و تکرار آن مطابقت ندارند'})}  />
+        
         <button>ثبت نام</button>
       </form>
        <p>
@@ -43,5 +71,6 @@ const submitHandler =()=>{
     </div>
   )
 }
+
 
 export default SignupPage
