61482d3cd3bbdd51dd81141f70499a4407b7f79d
Zahra shahriaripour <zahra.shahriaripour8285@gmail.com>
2025-11-16 22:42:39 +0330

    customdate picker component is complited

--- src/components/module/datepicker/CustomDatePicker.tsx
+++ src/components/module/datepicker/CustomDatePicker.tsx
@@ -1,21 +1,35 @@
+import DatePicker, { DateObject } from "react-multi-date-picker";
+import persian_fa from "react-date-object/locales/persian_fa";
+import persian from "react-date-object/calendars/persian";
 import { ProfileDataType } from "@/utils/types/Types";
-import styles from "./CustomDatePicker.module.css"
-import DatePicker from "react-multi-date-picker"
-import React from 'react'
+import styles from "./CustomDatePicker.module.css";
+import React from "react";
 
 
-type propsType={
-    profileData: ProfileDataType;
-      setProfileData: React.Dispatch<React.SetStateAction<ProfileDataType>>;
+type propsType = {
+  profileData: ProfileDataType;
+  setProfileData: React.Dispatch<React.SetStateAction<ProfileDataType>>;
+};
+
+function CustomDatePicker({ profileData, setProfileData }: propsType) {
+
+const changeHandler=(e: DateObject | null)=>{
+const date = new Date(e);
+setProfileData({...profileData,constructionDate:date})
 }
 
-function CustomDatePicker({profileData,setProfileData}:propsType) {
   return (
     <div className={styles.container}>
-        <p>تاریخ ساخت</p>
-        <DatePicker/>
+      <p>تاریخ ساخت</p>
+      <DatePicker
+      value={profileData.constructionDate }
+      onChange={changeHandler}
+        calendar={persian}
+        locale={persian_fa}
+        calendarPosition="bottom-right"
+      />
     </div>
-  )
+  );
 }
 
-export default CustomDatePicker
+export default CustomDatePicker;
